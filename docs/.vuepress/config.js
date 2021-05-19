7f90e5c492dfda2967e950c4be626ee231a4a787
hejf <hejf@guoguokeji.com.cn>
2021-05-19 13:43:03 +0800

    feat: loading

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,7 +34,8 @@ module.exports = {
             { title: "一个导出pdf的方案", path: "/page1/exportPdf" },
             { title: "前端自动化发布", path: "/page1/scp2" },
             { title: "vue中动态绑定hover效果", path: "/page1/hover" },
-            { title: "如何防止重复发送ajax请求", path: "/page1/axios" }
+            { title: "如何防止重复发送ajax请求", path: "/page1/axios" },
+            { title: "多个请求下 loading 的展示与关闭", path: "/page1/loading" }
           ],
         },
       ],
